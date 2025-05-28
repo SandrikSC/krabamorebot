feb4e66cfc5325db05472500a92a061827e8afdd
SandrikSC <pivowar-89@mail.ru>
2025-05-28 13:21:49 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,39 @@
+﻿import logging
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
+
+API_TOKEN = '7606185382:AAEvBBq3oBgCw3cbbOxEWs74DK1IiCwfCKg'
+WEBHOOK_HOST = 'https://krabamorebot.onrender.com'
+WEBHOOK_PATH = '/webhook'
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = int(os.getenv('PORT', 5000))
+
+bot = Bot(API_TOKEN, parse_mode='HTML')
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+
+main_menu = types.ReplyKeyboardMarkup(resize_keyboard=True).add("📦 Каталог", "📞 Контакты")
+
+@dp.message_handler(commands=['start'])
+async def start_cmd(message: types.Message):
+    await message.answer("👋 Добро пожаловать!", reply_markup=main_menu)
+
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+
+async def on_shutdown(dp):
+    await bot.delete_webhook()
+
+if __name__ == '__main__':
+    start_webhook(
+        dispatcher=dp,
+        webhook_path=WEBHOOK_PATH,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=True,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT
+    )
